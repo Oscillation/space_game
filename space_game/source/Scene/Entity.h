3e325b115455ea566f08b52e12b84b9119a07640
Mntndew <patrik.pihlstrom@gmail.com>
2014-03-21 09:18:28 +0100

    Made the entity update function virtual.

--- space_game/source/Scene/Entity.h
+++ space_game/source/Scene/Entity.h
@@ -8,7 +8,7 @@ public:
 	Entity();
 	virtual ~Entity();
 
-	void updateSelf(sf::Time const& p_deltaTime);
+	virtual void updateSelf(sf::Time const& p_deltaTime);
 
 	void drawSelf(sf::RenderTarget& target, sf::RenderStates states) const;
 };
